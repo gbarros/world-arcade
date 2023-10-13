7e93267598b021548cd56d3c06ceff3f4bf33ff2
Gabriel Barros <gabriel.barros@cartesi.io>
2023-10-12 23:54:11 -0300

    Attempt at fixing config

--- next-js-frontend/next.config.js
+++ next-js-frontend/next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir:'../docs',
-  assetPrefix: '.',
-  output: 'export',
+  //distDir:'../docs',
+  //assetPrefix: '.',
+  //output: 'export',
 }
 
 module.exports = nextConfig
