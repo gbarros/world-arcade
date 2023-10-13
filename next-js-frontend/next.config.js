97c1ed8fdfe413f657a1c9906297e9978878c750
Gabriel Barros <gabriel.barros@cartesi.io>
2023-10-13 00:35:01 -0300

    Add vercel config

--- next-js-frontend/next.config.js
+++ next-js-frontend/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  //distDir:'../docs',
-  //output: 'export',
+  distDir:'../public',
+  output: 'export',
   //assetPrefix: '.',
 }
 
