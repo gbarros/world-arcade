e2cfbb43c2d6358a93eca3facc50a4f754294113
Gabriel Barros <gabriel.barros@cartesi.io>
2023-10-14 22:24:30 -0400

    Add explorer and logs to the page

--- next-js-frontend/src/components/header.tsx
+++ next-js-frontend/src/components/header.tsx
@@ -3,18 +3,19 @@ import { useEffect, useState } from "react";
 import { Button, Nav, Navbar, Stack } from "react-bootstrap";
 import { GiDisc } from "react-icons/gi";
 
-export default function Header({activeKey}: {activeKey:string}) {
+export default function Header({ activeKey }: { activeKey: string }) {
+    if (!process.env.NEXT_PUBLIC_EXPLORER_URL) throw new Error("Undefined explorer url.");
     const [{ wallet, connecting }, connect, disconnect] = useConnectWallet()
     const [button, setButton] = useState(<></>);
 
     useEffect(() => {
         setButton((
             <Button variant="outline-light" className="position-absolute bottom-0 end-0 m-2"
-            onClick={() => (
-                wallet?
-                    disconnect(wallet)
-                :
-                    connect())}>
+                onClick={() => (
+                    wallet ?
+                        disconnect(wallet)
+                        :
+                        connect())}>
                 {connecting ? 'Connecting' : wallet ? 'Disconnect' : 'Connect Wallet'}
             </Button>
         ))
@@ -48,16 +49,22 @@ export default function Header({activeKey}: {activeKey:string}) {
                     </Nav.Item>
 
                     <Nav.Item>
-                        <Nav.Link href="/upload">Upload<span className="ms-1"><GiDisc/></span></Nav.Link>
+                        <Nav.Link href="/upload">Upload<span className="ms-1"><GiDisc /></span></Nav.Link>
                     </Nav.Item>
-{/*
+                    {/*
                     <Nav.Item>
                         <Nav.Link href="/how_to">How To</Nav.Link>
                     </Nav.Item>
                 */}
-                <Nav.Item>
-                    <Nav.Link href="/about">About</Nav.Link>
-                </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/about">About</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link target="_blank" href={process.env.NEXT_PUBLIC_EXPLORER_URL}>Explorer</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link target="_blank" href="http://172.233.201.228:8888/">Logs</Nav.Link>
+                    </Nav.Item>
                 </Nav>
 
             </Stack>
--- next-js-frontend/src/pages/about.tsx
+++ next-js-frontend/src/pages/about.tsx
@@ -45,9 +45,18 @@ export default function About() {
             <p>But in order to do that you&#39;d need to reset your metamask wallet.</p>
           </li>
           <li>
-            <p>Import some private keys from that seed, here is a recommended account:</p>
+            <p>Import some private keys from that seed, here are a few accounts:</p>
             <blockquote>
-              <p>0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80</p>
+              <p>0. 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80</p>
+              <p>1. 0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d</p>
+              <p>2. 0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a</p>
+              <p>3. 0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6</p>
+              <p>4. 0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a</p>
+              <p>5. 0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba</p>
+              <p>6. 0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e</p>
+              <p>7. 0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356</p>
+              <p>8. 0xdbda1821b80551c9d65939329250298aa3472ba22feea921c0cf5d620ea67b97</p>
+              <p>9. 0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6</p>
             </blockquote>
           </li>
         </ol>
@@ -60,7 +69,7 @@ export default function About() {
             <p>Chain ID: 0x7A69</p>
           </blockquote>
           <li>Click button to automatically add it. 
-            <Button variant="outline-light" className="text-center" onClick={() => { addRemoteDevNet() }}>
+            <Button variant="outline-dark" className="bottom-0 end-0 m-2" onClick={() => { addRemoteDevNet() }}>
               Add Network to Metask
             </Button>
           </li>
